5f086779d851df1b4fe8fe028fc13a20ff005a96
Hossein <hossien2005@gmail.com>
2020-06-17 21:23:29 +0430

    Some Sample for Firebase DataBase

--- app/src/main/java/com/university/firebase2/MainActivity.java
+++ app/src/main/java/com/university/firebase2/MainActivity.java
@@ -1,18 +1,41 @@
 package com.university.firebase2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button logout;
+    private EditText edit;
+    private Button add;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         logout = findViewById(R.id.logout);
+        edit = findViewById(R.id.edit);
+        add = findViewById(R.id.add);
+        listView = findViewById(R.id.listView);
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +53,57 @@ public class MainActivity extends AppCompatActivity {
                 FirebaseAuth.getInstance().signOut();
                 Toast.makeText(MainActivity.this , "شما با موفقیت خارج شدید" , Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(MainActivity.this , StartActivity.class));
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_name = edit.getText().toString();
+                if (txt_name.isEmpty()) {
+                    Toast.makeText(MainActivity.this,"نام نباید خالی باشد",Toast.LENGTH_SHORT).show();
+                } else {
+                    FirebaseDatabase.getInstance().getReference().child("Class").push().child("Name").setValue(txt_name);
+                }
+            }
+        });
+
+        final ArrayList<String> list = new ArrayList<>();
+        final ArrayAdapter adapter = new ArrayAdapter<String>(this , R.layout.list_item , list);
+        listView.setAdapter(adapter);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Information");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Information info = snapshot.getValue(Information.class);
+                    String txt = info.getName() + " : " + info.getEmail();
+                    list.add(txt);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        DocumentReference docRef = FirebaseFirestore.getInstance().collection("citie222s").document("Tehran");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot doc = task.getResult();
+                    if (doc.exists()) {
+                        Log.d("Document", doc.getData().toString());
+                    } else {
+                        Log.d("Document", "No Data");
+                    }
+                }
             }
         });
     }
